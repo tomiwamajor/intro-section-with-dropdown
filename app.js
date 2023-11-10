d9603d43c0e2f54ce02ef2c6d74dd6fa661f8a7f
Adelekun Adetomiwa Kolapo <adetomiwaadelekun@gmail.com>
2023-11-10 15:38:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+"strict";
+
+const menu = document.querySelector(".menu");
+const dropdown = document.querySelectorAll(".dropdown");
+
+menu.addEventListener("click", function () {
+  menu.parentElement.classList.toggle("open");
+  document.body.classList.toggle("nav-open");
+});
+dropdown.forEach(function (item) {
+  item.addEventListener("click", function () {
+    item.parentElement.classList.toggle("link-open");
+  });
+});
